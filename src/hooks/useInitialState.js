7d0b4e60cedd3007bb3d4419f7da59d93711b6da
joselacruz <joselacruzperez@gmail.com>
2023-08-01 21:39:52 -0400

    create account

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,6 +1,5 @@
-import {useState } from 'react';
-import axios from 'axios';
-
+import { useState } from "react";
+import axios from "axios";
 
 // const initialState = {
 //     cart: [],
@@ -9,112 +8,104 @@ import axios from 'axios';
 let initialState;
 const itemInLocalStorange = JSON.parse(localStorage.getItem("cart"));
 
-if(itemInLocalStorange) {
+if (itemInLocalStorange) {
   initialState = itemInLocalStorange;
-}
-else{
+} else {
   initialState = {
     cart: [],
-  
-  }
+  };
 }
 
 const useInitialState = () => {
-  
-    const [state, setState] = useState(initialState);
-    const existItem = (itemId) => {
-        return state.cart.some(item => item.id === itemId);
-      };
-    const addToCart =   (payload) => {
-        const itemId = payload.id;
- 
+  const [state, setState] = useState(initialState);
+  const existItem = (itemId) => {
+    return state.cart.some((item) => item.id === itemId);
+  };
+  const addToCart = (payload) => {
+    const itemId = payload.id;
+
     // Verificamos si el artículo ya está en el carrito
     const itemExist = existItem(itemId);
     if (itemExist) {
-        // Si el artículo ya existe en el carrito, actualizamos la cantidad utilizando el hook personalizado
-        setState((prevState) => ({
-          ...prevState,
-          cart: prevState.cart.map(item =>
-            item.id === itemId ? { ...item, cantidad: item.cantidad + 1 } : item
-            
-          )
-        }));
-   
-    
+      // Si el artículo ya existe en el carrito, actualizamos la cantidad utilizando el hook personalizado
+      setState((prevState) => ({
+        ...prevState,
+        cart: prevState.cart.map((item) =>
+          item.id === itemId ? { ...item, cantidad: item.cantidad + 1 } : item
+        ),
+      }));
+    } else {
+      // Si el artículo no está en el carrito, lo agregamos con cantidad 1 utilizando el hook personalizado
+      setState((prevState) => ({
+        ...prevState,
+        cart: [...prevState.cart, { ...payload, cantidad: 1 }],
+      }));
+    }
+  };
 
-      } else {
-        // Si el artículo no está en el carrito, lo agregamos con cantidad 1 utilizando el hook personalizado
-        setState((prevState) => ({ ...prevState, cart: [...prevState.cart, { ...payload, cantidad: 1 }] }));
-  
-      }
-  
-
-};
-
-const calcTotalPriceCart = (array) => {
+  const calcTotalPriceCart = (array) => {
     return array.reduce((total, item) => total + item.price * item.cantidad, 0);
   };
 
   const totalItems = (array) => {
-    return array.map((item) => item.cantidad).reduce((acumulador, numero) => 
-     acumulador + numero, 0);
-  }
+    return array
+      .map((item) => item.cantidad)
+      .reduce((acumulador, numero) => acumulador + numero, 0);
+  };
 
-const removeFromCart = (payload) => {
+  const removeFromCart = (payload) => {
     setState({
       ...state,
-      cart: state.cart.filter(items => items.id !== payload.id),
+      cart: state.cart.filter((items) => items.id !== payload.id),
     });
+  };
 
- 
-}
+  const addLocalStorange = () => {
+    const dataLocalstorange = JSON.parse(localStorage.getItem("cart"));
+    const newObj = { ...state.cart };
+    if (dataLocalstorange.ordenes) {
+      const newOrdenes = [...state.ordenes, [...state.cart]];
 
-const addLocalStorange = () => {
-  const dataLocalstorange = JSON.parse(localStorage.getItem("cart"));
-  const newObj = {...state.cart};
-  if(dataLocalstorange.ordenes) {
-    const newOrdenes = [...state.ordenes,[...state.cart]];
-   
+      //si ya hay ordenes respetamos la anteriores
+      localStorage.setItem(
+        "cart",
+        JSON.stringify({
+          cart: [],
+          ordenes: newOrdenes,
+          user: state?.user,
+        })
+      );
 
-		//si ya hay ordenes respetamos la anteriores
-		localStorage.setItem('cart',JSON.stringify({
-			cart: [],
-			ordenes: newOrdenes,
-      user:state?.user,
-    }));
-
-	//de lo contrario agregamos al objecto una propiedad ordenes	
-	} 
-
-  else{
-    localStorage.setItem('cart',JSON.stringify({
-			cart: [],
-			ordenes: [[...state.cart]],
-      user:state?.user,
-    }));
-  }
-
-}
-
-const getInfoProfile = async() => {
-if(state.user) 
-{
-  const response = await axios(`https://api.escuelajs.co/api/v1/users/${state.user}`);
-  return response.data;
-}
-
-
-}
-    return {
-        state,
-        addToCart,
-        removeFromCart,
-        calcTotalPriceCart,
-        addLocalStorange,
-        totalItems,
-        getInfoProfile,
+      //de lo contrario agregamos al objecto una propiedad ordenes
+    } else {
+      localStorage.setItem(
+        "cart",
+        JSON.stringify({
+          cart: [],
+          ordenes: [[...state.cart]],
+          user: state?.user,
+        })
+      );
     }
+  };
 
-}
+  const getInfoProfile = async () => {
+    if (state.user) {
+      const response = await axios(
+        `https://api.escuelajs.co/api/v1/users/${state.user}`
+      );
+      return response.data;
+    }
+  };
+  return {
+    state,
+    addToCart,
+    removeFromCart,
+    calcTotalPriceCart,
+    addLocalStorange,
+    totalItems,
+    getInfoProfile,
+  };
+};
 
 export default useInitialState;
--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,56 +1,129 @@
-import React,{useRef} from 'react';
-import axios from 'axios';
-import '@styles/CreateAccount.scss';
+import React, { useRef, useState } from "react";
+import axios from "axios";
+import "@styles/CreateAccount.scss";
+import ErrorMessage from "@components/ErrorMessage";
 
 const CreateAccount = () => {
-	const form = useRef(null);
+  const form = useRef(null);
+  const [showMessage, setShowMessage] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
 
-    const register = (e) => {
-     e.preventDefault();
-	 const formData  = new FormData(form.current);
+  const register = (e) => {
+    setIsClicked(true); //desactivamos el onclick para evitar repetir ordenes
+    e.preventDefault();
 
-	 const data = {
-		name: formData.get('name'),
-        email: formData.get('email'),
-		password: formData.get('password'),
-		avatar: "https://api.lorem.space/image/face?w=640&h=480&r=867",
-	 }
+    const formData = new FormData(form.current);
+    const data = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      avatar: "https://api.lorem.space/image/face?w=640&h=480&r=867",
+    };
 
-    const sendData = async () =>{
-		try {
-			const response = await axios.post('https://api.escuelajs.co/api/v1/users/', data);
-			console.log('Respuesta de la API:', response.data);
-            //repuesta exitosa enviame a login
-			// window.location = "/login";
-		  } catch (error) {
-			console.error('Error al enviar los datos:', error.message);
+    //si algun campo esta vacio mostramos ese error en ErrorMessage
+    if (!data.name || !data.email || !data.password) {
+      setShowMessage("Todos los campos deben ser completados");
+      setIsClicked(false);
+    } else {
+      //si Todo esta en orden chequeamos si el correo ya esta en uso
+      checkAvailableEmail(data);
+    }
+  };
 
-		  }
-	}
-	sendData();
- 	 console.log(data);
-	}
+  const checkAvailableEmail = async (data) => {
+    const response = await axios("https://api.escuelajs.co/api/v1/users");
+    const emailUse = response.data.some((item) => item.email === data.email);
+    //si el correo no esta hacemos el registo
+    if (!emailUse) {
+      sendData(data);
+    } else {
+      setShowMessage("Este Correo ya esta en uso");
+      setIsClicked(false);
+    }
+  };
 
-	return (
-		<div className="CreateAccount">
-			<div className="CreateAccount-container">
-				<h1 className="title">My account</h1>
-				<form action="/" className="form" ref={form}>
-					<div>
-						<label htmlFor="name" className="label">Name</label>
-						<input type="text" name="name" placeholder="Teff" className="input input-name" />
-						<label htmlFor="email" className="label">Email</label>
-						<input type="text" name="email" placeholder="platzi@example.com" className="input input-email" />
-						<label htmlFor="password" className="label">Password</label>
-						<input type="password" name="password" placeholder="*********" className="input input-password" />
-					</div>
-					<input type="submit" value="Create" className="primary-button login-button" 
-					onClick={register}
-					/>
-				</form>
-			</div>
-		</div>
-	);
-}
+  const sendData = async (data) => {
+    try {
+      const response = await axios.post(
+        "https://api.escuelajs.co/api/v1/users/",
+        data
+      );
+    } catch (error) {
+      console.error("Error al enviar los datos:", error.message);
+    } finally {
+      //repuesta exitosa enviame a login
+      window.location = "/login";
+    }
+  };
+
+  const handleInputEmail = (event) => {
+    const { name, value } = event.target;
+    const emailRegex =
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|org|net|es|edu|gov|mil|io|info|etc)$/;
+    if (!emailRegex.test(value)) {
+      event.target.setCustomValidity("Ingresa un correo electrónico válido.");
+    } else {
+      event.target.setCustomValidity("");
+    }
+  };
+
+  const handleInput = (event) => {
+    const { value } = event.target;
+    if (value.length < 4) {
+      event.target.setCustomValidity("deber ser mayor a 4 caracteres");
+    } else {
+      event.target.setCustomValidity("");
+    }
+  };
+
+  return (
+    <div className="CreateAccount">
+      <div className="CreateAccount-container">
+        <h1 className="title">My account</h1>
+        <form action="/" className="form" ref={form} onSubmit={register}>
+          <div>
+            <label htmlFor="name" className="label">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Teff"
+              className="input input-name"
+              onChange={handleInput}
+            />
+            <label htmlFor="email" className="label">
+              Email
+            </label>
+            <input
+              type="text"
+              name="email"
+              placeholder="platzi@example.com"
+              className="input input-email"
+              onChange={handleInputEmail}
+            />
+            <label htmlFor="password" className="label">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="*********"
+              className="input input-password"
+              onChange={handleInput}
+            />
+          </div>
+          <input
+            type="submit"
+            value="Create"
+            className="primary-button login-button"
+            disabled={isClicked}
+          />
+        </form>
+        {showMessage && <ErrorMessage message={showMessage} />}
+      </div>
+    </div>
+  );
+};
 
 export default CreateAccount;
